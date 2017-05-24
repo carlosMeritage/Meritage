c065f69f9fd9ee30cd7f11ee8298302b76cdd231
Carlos Cordoba <carlos.cordoba@meritagehomes.com>
2017-05-24 09:24:01 -0700

    updating the GitScript file

--- GitScriptingMay24.ps1
+++ GitScriptingMay24.ps1
@@ -217,4 +217,88 @@ Date:   Tue May 23 15:28:34 2017 -0700
 
     this is just a test commit
 
+PS C:\GitRepository> git add *.*
+
+PS C:\GitRepository> git commit -m "This includes the Git Script file"
+[master 62e6705] This includes the Git Script file
+ 1 file changed, 220 insertions(+)
+ create mode 100644 GitScriptingMay24.ps1
+
+PS C:\GitRepository> git log
+commit 62e6705df3a92efa9b7d96c2bab7fdf505d51bf0
+Author: Carlos Cordoba <carlos.cordoba@meritagehomes.com>
+Date:   Wed May 24 08:27:06 2017 -0700
+
+    This includes the Git Script file
+
+commit c884e9dff2013e63e2bd0057b9cc1d5c77f93c73
+Author: Carlos Cordoba <carlos.cordoba@meritagehomes.com>
+Date:   Wed May 24 08:12:56 2017 -0700
+
+    V2.0 additional changes
+
+commit 0ee638794a6d651ea0f98ae0476f03d6cdc4a9cf
+Author: Carlos Cordoba <carlos.cordoba@meritagehomes.com>
+Date:   Wed May 24 08:10:18 2017 -0700
+
+    V2.0 changes
+
+commit cfeb4a7eb430efd751742094f39fca4e814a7893
+Author: Carlos Cordoba <carlos.cordoba@meritagehomes.com>
+Date:   Tue May 23 15:58:09 2017 -0700
+
+    this is the 3rd commit
+
+commit 1b7bd351d5cd33cbabd63f2dc2866c9603f540af
+Author: Carlos Cordoba <carlos.cordoba@meritagehomes.com>
+Date:   Tue May 23 15:30:35 2017 -0700
+
+    this is the 2nd commit
+
+commit 52989f13f53c90aeb2c6129a853e15c081a5620b
+Author: Carlos Cordoba <carlos.cordoba@meritagehomes.com>
+Date:   Tue May 23 15:28:34 2017 -0700
+
+    this is just a test commit
+
+PS C:\GitRepository> git remote add origin https://github.com/carlosMeritage/Meritage.git
+
+PS C:\GitRepository> git push -u origin master
+git : Logon failed, use ctrl+c to cancel basic credential prompt.
+At line:1 char:1
++ git push -u origin master
++ ~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : NotSpecified: (Logon failed, u...dential prompt.:String) 
+    [], RemoteException
+    + FullyQualifiedErrorId : NativeCommandError
+ 
+bash: /dev/tty: No such device or address
+error: failed to execute prompt script (exit code 1)
+fatal: could not read Username for 'https://github.com/': No error
+
+PS C:\GitRepository> git push -u origin master
+git : Logon failed, use ctrl+c to cancel basic credential prompt.
+At line:1 char:1
++ git push -u origin master
++ ~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : NotSpecified: (Logon failed, u...dential prompt.:String) 
+    [], RemoteException
+    + FullyQualifiedErrorId : NativeCommandError
+ 
+bash: /dev/tty: No such device or address
+error: failed to execute prompt script (exit code 1)
+fatal: could not read Username for 'https://github.com/': No error
+
+PS C:\GitRepository> git push -u origin master
+Branch master set up to track remote branch master from origin.
+git : To https://github.com/carlosMeritage/Meritage.git
+At line:1 char:1
++ git push -u origin master
++ ~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : NotSpecified: (To https://gith...ge/Meritage.git:String) 
+    [], RemoteException
+    + FullyQualifiedErrorId : NativeCommandError
+ 
+ * [new branch]      master -> master
+
 PS C:\GitRepository> 
